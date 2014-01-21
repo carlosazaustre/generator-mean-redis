3b26932e2adae9098d4a8a4f10ea4c1429667653
Carlos Azaustre <cazaustre@gmail.com>
2014-01-21 20:46:08 +0100

    routes added

---
+++ app/controllers/users.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var mongoose 	= require('mongoose')
+,	User		= mongoose.model('User');
+
+// Auth Callback
+exports.authCallback = function(req, res, next) {
+	res.redirect('/');
+};
+
+// Login Form
+exports.login = function(req, res) {
+	res.render('users/login', {
+		title: 'Login',
+	});
+};
+
+// Register Form
+exports.register = function(req, res) {
+	res.render('users/register'), {
+		title: 'Register'
+	});
+};
+
+// Logout
+exports.logout = function(req, res) {
+	req.logout();
+	res.redirect('/');
+};
+
+// Session
+exports.session = function(req, res) {
+	res.redirect('/');
+};
+
+// User Profile
+exports.profile = function(req, res) {
+	var user = req.profile;
+
+	res.render('users/profile', {
+		title: user.name,
+		user: user
+	});
+};
+
+// Send User
+exports.me = function(req, res) {
+	res.jsonp(req.user || null);
+};
+
+// Find user by id
+exports.user = function(req, res, next, id) {
+	User
+		.findOne({
+			_id: id
+		})
+		.exec(function(err, user) {
+			if(err) {
+				return next(err);
+			}
+			if(!user) {
+				return next(new Error('Failed to load User' + id));
+			}
+			req.profile = user;
+			next();
+		});	
+};
--- server.js
+++ server.js
@@ -5,12 +5,14 @@ var express 	= require('express')
 ,	passport 	= require('passport')
 , 	mongoose 	= require('mongoose')
 , 	logger 		= require('mean-logger')
-, 	fs			= require('fs');
+, 	fs			= require('fs')
 
-// TODO añadir config, auth, y ENV
+,	env 		= process.env.NODE_ENV || 'development'
+, 	config		= require('./config/config');
+// TODO añadir auth middlewares
 
 // BD Connection
-var db = mongoose.connect('');
+var db = mongoose.connect(config.db);
 
 // BD Models
 var modelsPath = __dirname + '/app/models/';
@@ -18,19 +20,19 @@ fs.readdirSync(modelsPath).forEach(function(file) {
 	require(modelsPath + file);
 });
 
-var app = express();
-
 // Express config settings
-require('./config/express')(app, passport);
+var app = express();
 require('./config/passport')(passport);
+require('./config/express')(app, passport);
+require('./config/routes')(app, passport, auth);
 // TODO añadir rutas
 
-var port = 3000;
+// Start app by listening at port and logger 
+var port = config.port;
 app.listen(port, function() {
 	console.log('Express server started on port ' + port);	
 });
 
-// Logger
 logger.init(app, passport, mongoose);
 
 exports = module.exports = app;
