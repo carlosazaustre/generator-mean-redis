77b1d2cb29f1b5becc9816c92e0ee7ddfe4cea4e
Carlos Azaustre <cazaustre@gmail.com>
2014-01-22 19:26:04 +0100

    database connection added

--- config/express.js
+++ config/express.js
@@ -5,12 +5,11 @@ var express = require('express')
 ,	swig	= require('swig');
 
 module.exports = function(app) {
-	app.use(express.logger('dev'));
+	app.use(express.logger('dev'));	
+	//showstack error
+	//compress
 
 	app.use(express.favicon());
-	app.use(express.cookieParser());
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
 	app.use(express.static(path.join(__dirname, '../public')));
 
 	app.engine('html', swig.renderFile);
@@ -20,4 +19,13 @@ module.exports = function(app) {
 	app.set('port', 3000);
 
 	app.enable('jsonp callback');
+
+	app.configure(function() {
+		app.use(express.cookieParser());
+		app.use(express.bodyParser());
+		app.use(express.methodOverride());
+		// session
+		// passport session
+		app.use(app.router);
+	});
 };
--- server.js
+++ server.js
@@ -1,16 +1,53 @@
 'use strict';
 
 // Module dependencies.
-var express 	= require('express');
+var express 	= require('express')
+,	fs			= require('fs')
+,	path 		= require('path')
+,	mongoose	= require('mongoose');
 
 var app = express();
 
 // Express settings
 require('./config/express')(app);
 
-app.get('/', function(req, res) {
-	res.render('index', {});
-});
+// DB Connection
+var db = mongoose.connect('http://localhost:27017/mean-dev');
+
+// Models
+var modelsPath = path.join(__dirname, 'app/models');
+var walk = function(path) {
+	fs.readdirSync(path).forEach(function(file) {
+		var newPath = path + '/' + file;
+		var stat = fs.statSync(newPath);
+		if(stat.isFile()) {
+			if (/(.*)\.(js$|coffee$)/.test(file)) {
+                require(newPath);
+            }
+		} else if(stat.isDirectory()) {
+			walk(newPath);
+		}
+	});
+};
+walk(modelsPath);
+
+// Routes
+var routesPath = path.join(__dirname, 'app/routes');
+var walk = function(path) {
+	fs.readdirSync(path).forEach(function(file) {
+		var newPath = path + '/' + file;
+		var stat = fs.statSync(newPath);
+		if(stat.isFile()) {
+			if (/(.*)\.(js$|coffee$)/.test(file)) {
+                require(newPath)(app); //passport
+            }
+        // Middlewares is not a route itself    
+		} else if(stat.isDirectory() && file !== 'middlewares') {
+			walk(newPath);
+		}
+	});
+};
+walk(routesPath); 
 
 app.listen(app.get('port'), function() {
 	console.log('Express App listening at port ' + app.get('port'));
