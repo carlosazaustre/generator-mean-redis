820d10d86dee7167a7e5e499d2408eed0f74ac41
Carlos Azaustre <cazaustre@gmail.com>
2014-01-22 17:40:34 +0100

    initial commit

--- app/controllers/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.render = function(req, res) {
-	//res.render('index', {
-	//	user: req.user ? JSON.stringify(req.user) : 'null'
-	//});
-
-	res.render('index', {});
-};
--- server.js
+++ server.js
@@ -1,37 +1,10 @@
 'use strict';
 
 // Module dependencies.
-var express 	= require('express')
-,	passport 	= require('passport')
-, 	mongoose 	= require('mongoose')
-, 	logger 		= require('mean-logger')
-, 	fs			= require('fs')
-, 	path 		= require('path')
+var express 	= require('express');
 
-,	env 		= process.env.NODE_ENV || 'development'
-, 	config		= require('./config/config')
-,	auth		= require('./config/middlewares/authorization');
+var app = express()
 
-// BD Connection
-var db = mongoose.connect(config.db);
-
-// BD Models
-var modelsPath = path.join(config.root, '/app/models/');
-fs.readdirSync(modelsPath).forEach(function(file) {
-	require(modelsPath + file);
+app.listen('3000', function() {
+	console.log('Express App listening at http://localhost:3000');
 });
-
-// Express config settings
-var app = express();
-require('./config/passport')(passport);
-require('./config/express')(app, passport);
-require('./config/routes')(app, passport, auth);
-
-// Start app by listening at port and logger 
-app.listen(config.port, function() {
-	console.log('Express server started on port ' + config.port);	
-});
-
-logger.init(app, passport, mongoose);
-
-exports = module.exports = app;
