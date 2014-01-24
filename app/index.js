de200e2968762840233473dd6aa36b270dee93ca
Carlos Azaustre <cazaustre@gmail.com>
2014-01-24 11:40:33 +0100

    preparing to yeoman generator

--- app/index.js
+++ app/index.js
@@ -1,2 +1,54 @@
 'use strict';
 
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var MeanRedisGenerator = module.exports = function MeanRedisGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(MeanRedisGenerator, yeoman.generators.Base);
+
+MeanRedisGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [{
+    type: 'confirm',
+    name: 'someOption',
+    message: 'Would you like to enable this option?',
+    default: true
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.someOption = props.someOption;
+
+    cb();
+  }.bind(this));
+};
+
+MeanRedisGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/templates');
+
+  this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+  this.copy('gruntfile.js', 'gruntfile.js');
+  this.copy('server.js', 'server.js');
+  this.copy('README.md', 'README.md');
+};
+
+MeanRedisGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
